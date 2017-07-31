ccfdf750e4b8702b17edc2c5fa83039546948477
tonykung <tonykung06@gmail.com>
2017-08-01 01:30:18 +0800

    some code formatting

--- src/SamuraiAppCore.Data/DisconnectedData.cs
+++ src/SamuraiAppCore.Data/DisconnectedData.cs
@@ -6,68 +6,73 @@ using SamuraiAppCore.Domain;
 
 namespace SamuraiAppCore.Data
 {
-  public class DisconnectedData
-  {
-    private SamuraiContext _context;
-
-    public DisconnectedData(SamuraiContext context) {
-      _context = context;
-      _context.ChangeTracker.QueryTrackingBehavior
-        = QueryTrackingBehavior.NoTracking;
-    }
-
-    public List<KeyValuePair<int, string>> GetSamuraiReferenceList() {
-      var samurais = _context.Samurais.OrderBy(s => s.Name)
-        .Select(s => new {s.Id, s.Name})
-        .ToDictionary(t => t.Id, t => t.Name).ToList();
-      return samurais;
-    }
-
-  
-    public Samurai LoadSamuraiGraph(int id) {
-      var samurai =
-        _context.Samurais
-        .Include(s => s.SecretIdentity)
-        .Include(s => s.Quotes)
-        .FirstOrDefault(s => s.Id == id);
-     return samurai;
-    }
-
-
-    public void SaveSamuraiGraph(Samurai samurai)
+    public class DisconnectedData
     {
-      _context.ChangeTracker.TrackGraph
-        (samurai, e=>ApplyStateUsingIsKeySet(e.Entry));
-      _context.SaveChanges();
-    }
+        private SamuraiContext _context;
 
-    private static void ApplyStateUsingIsKeySet(EntityEntry entry) {
-      if (entry.IsKeySet) {
-        if (((ClientChangeTracker) entry.Entity).IsDirty) {
-          entry.State = EntityState.Modified;
+        public DisconnectedData(SamuraiContext context)
+        {
+            _context = context;
+            _context.ChangeTracker.QueryTrackingBehavior
+              = QueryTrackingBehavior.NoTracking;
         }
-        else {
-          entry.State = EntityState.Unchanged;
+
+        public List<KeyValuePair<int, string>> GetSamuraiReferenceList()
+        {
+            var samurais = _context.Samurais.OrderBy(s => s.Name)
+              .Select(s => new { s.Id, s.Name })
+              .ToDictionary(t => t.Id, t => t.Name).ToList();
+            return samurais;
         }
-      }
-      else {
-        entry.State = EntityState.Added;
-      }
+
+
+        public Samurai LoadSamuraiGraph(int id)
+        {
+            var samurai =
+              _context.Samurais
+              .Include(s => s.SecretIdentity)
+              .Include(s => s.Quotes)
+              .FirstOrDefault(s => s.Id == id);
+            return samurai;
+        }
+
+
+        public void SaveSamuraiGraph(Samurai samurai)
+        {
+            _context.ChangeTracker.TrackGraph
+              (samurai, e => ApplyStateUsingIsKeySet(e.Entry));
+            _context.SaveChanges();
+        }
+
+        private static void ApplyStateUsingIsKeySet(EntityEntry entry)
+        {
+            if (entry.IsKeySet)
+            {
+                if (((ClientChangeTracker)entry.Entity).IsDirty)
+                {
+                    entry.State = EntityState.Modified;
+                }
+                else
+                {
+                    entry.State = EntityState.Unchanged;
+                }
+            }
+            else
+            {
+                entry.State = EntityState.Added;
+            }
+        }
+
+        public void DeleteSamuraiGraph(int id)
+        {
+            //goal:  delete samurai , quotes and secret identity
+            //       also delete any joins with battles
+            //EF Core supports Cascade delete by convention
+            //Even if full graph is not in memory, db is defined to delete
+            //But always double check!
+            var samurai = _context.Samurais.Find(id); //NOT TRACKING !!
+            _context.Entry(samurai).State = EntityState.Deleted; //TRACKING
+            _context.SaveChanges();
+        }
     }
-
-    public void DeleteSamuraiGraph(int id)
-    {
-      //goal:  delete samurai , quotes and secret identity
-      //       also delete any joins with battles
-      //EF Core supports Cascade delete by convention
-      //Even if full graph is not in memory, db is defined to delete
-      //But always double check!
-      var samurai = _context.Samurais.Find(id); //NOT TRACKING !!
-      _context.Entry(samurai).State=EntityState.Deleted; //TRACKING
-      _context.SaveChanges();
-
-
-
-    }
-  }
 }
--- src/SamuraiWebApi/Controllers/SamuraiController.cs
+++ src/SamuraiWebApi/Controllers/SamuraiController.cs
@@ -5,42 +5,47 @@ using SamuraiAppCore.Domain;
 
 namespace SamuraiWebApi.Controllers
 {
-  [Route("api/[controller]")]
-  public class SamuraiController : Controller
-  {
-    private DisconnectedData _repo;
-
-    public SamuraiController(DisconnectedData repo) {
-      _repo = repo;
-    }
-
-    [HttpGet]
-    public IEnumerable<KeyValuePair<int, string>> Get() {
-      var list= _repo.GetSamuraiReferenceList();
-      return list;
-    }
-
-    [HttpGet("{id}")]
-    public Samurai Get(int id)
+    [Route("api/[controller]")]
+    public class SamuraiController : Controller
     {
-      return _repo.LoadSamuraiGraph(id);
-    }
+        private DisconnectedData _repo;
 
-    //example raw json: {"name":"Julie","secretIdentity":{"realName":"Julia"}}
-    [HttpPost]
-    public void Post([FromBody] Samurai value) {
-      _repo.SaveSamuraiGraph(value);
-    }
+        public SamuraiController(DisconnectedData repo)
+        {
+            _repo = repo;
+        }
 
-    //example raw json:{"id":"3","name":"Julietta","IsDirty":"true","secretIdentity":{"id":"3","realName":"Julia Lerman","IsDirty":"true"}}
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] Samurai value) {
-      _repo.SaveSamuraiGraph(value);
-    }
+        [HttpGet]
+        public IEnumerable<KeyValuePair<int, string>> Get()
+        {
+            var list = _repo.GetSamuraiReferenceList();
+            return list;
+        }
 
-    [HttpDelete("{id}")]
-    public void Delete(int id) {
-      _repo.DeleteSamuraiGraph(id);
+        [HttpGet("{id}")]
+        public Samurai Get(int id)
+        {
+            return _repo.LoadSamuraiGraph(id);
+        }
+
+        //example raw json: {"name":"Julie","secretIdentity":{"realName":"Julia"}}
+        [HttpPost]
+        public void Post([FromBody] Samurai value)
+        {
+            _repo.SaveSamuraiGraph(value);
+        }
+
+        //example raw json:{"id":"3","name":"Julietta","IsDirty":"true","secretIdentity":{"id":"3","realName":"Julia Lerman","IsDirty":"true"}}
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Samurai value)
+        {
+            _repo.SaveSamuraiGraph(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repo.DeleteSamuraiGraph(id);
+        }
     }
-  }
 }
